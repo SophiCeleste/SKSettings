7f8fc6e01584ac3f21793d8cf8c327cfae10059d
Sophia Celeste <git@requiem.dev>
2024-02-28 00:24:25 -0600

    0.0.4h

--- actions.js
+++
@@ -1,9 +0,0 @@
-const actions = {}
-
-// Global actions
-
-// Scroll within current element
-// dir - scroll direction. Options: down | up | pageDown | fullPageDown | pageUp | fullPageUp | top | bottom | left | right | leftmost | rightmost | byRatio
-actions.scroll = dir => api.Normal.scroll(dir);
-
-export default actions
